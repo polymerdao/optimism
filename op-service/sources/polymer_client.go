c719ca1608ead5bdd7079e73eb4723a9ce5cc187
wenwei <wenwei@polymerlabs.org>
2023-10-17 14:35:13 -0400

    stub PolymerClient

---
+++ op-service/sources/polymer_client.go
@@ -0,0 +1,62 @@
+package sources
+
+import (
+	"context"
+	"math/big"
+
+	"github.com/ethereum-optimism/optimism/op-service/client"
+	"github.com/ethereum-optimism/optimism/op-service/eth"
+	"github.com/ethereum/go-ethereum/common"
+)
+
+// PolymerClient is a L2ClientGeneric implementation that interacts with the Polymer's ABCI app as an L2 Execution
+// Engine via RPC bindings.
+type PolymerClient struct {
+	client client.RPC
+}
+
+func (p *PolymerClient) PayloadByLabel(ctx context.Context, label eth.BlockLabel) (*eth.ExecutionPayload, error) {
+	var payload *eth.ExecutionPayload
+	err := p.client.CallContext(ctx, &payload, "ee_getPayloadByLabel", label)
+	return payload, err
+}
+
+func (p *PolymerClient) PayloadByNumber(ctx context.Context, num uint64) (*eth.ExecutionPayload, error) {
+	var payload *eth.ExecutionPayload
+	err := p.client.CallContext(ctx, &payload, "ee_getPayloadByNumber", num)
+	return payload, err
+}
+
+func (p *PolymerClient) PayloadByHash(ctx context.Context, hash common.Hash) (*eth.ExecutionPayload, error) {
+	var payload *eth.ExecutionPayload
+	err := p.client.CallContext(ctx, &payload, "ee_getPayloadByHash", hash)
+	return payload, err
+}
+
+func (p *PolymerClient) InfoByHash(ctx context.Context, hash common.Hash) (eth.BlockInfo, error) {
+	var info eth.BlockInfo
+	err := p.client.CallContext(ctx, &info, "ee_getInfoByHash", hash)
+	return info, err
+}
+
+func (p *PolymerClient) GetProof(ctx context.Context, address common.Address, storage []common.Hash, blockTag string) (*eth.AccountResult, error) {
+	var result *eth.AccountResult
+	err := p.client.CallContext(ctx, &result, "ee_getProof", address, storage, blockTag)
+	return result, err
+}
+
+func (p *PolymerClient) InfoByLabel(ctx context.Context, label eth.BlockLabel) (eth.BlockInfo, error) {
+	var info eth.BlockInfo
+	err := p.client.CallContext(ctx, &info, "ee_getInfoByLabel", label)
+	return info, err
+}
+
+func (p *PolymerClient) ChainID(ctx context.Context) (*big.Int, error) {
+	var chainID *big.Int
+	err := p.client.CallContext(ctx, &chainID, "ee_getChainID")
+	return chainID, err
+}
+
+func (p *PolymerClient) Close() {
+	p.client.Close()
+}
