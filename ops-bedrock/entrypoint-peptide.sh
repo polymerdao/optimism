8215a05759fb5b95efb5db5a4caf35b5bc9d448e
Derek <derek@polymerlabs.org>
2023-11-09 15:29:04 -0800

    update peptide to use init image and script

---
+++ ops-bedrock/entrypoint-peptide.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -exu
+
+PEPTIDE_GENESIS_FILE='/root/.peptide/config/genesis.json'
+L1_HASH=$(jq -r .genesis.l1.hash /rollup.json)
+
+ls -la /root/.peptide
+
+if [ ! -r "${PEPTIDE_GENESIS_FILE}" ]; then
+	echo "${PEPTIDE_GENESIS_FILE} missing, running 'init' and 'seal'"
+	echo "Initializing genesis."
+    peptide init --l1-hash="${L1_HASH}" --l1-height=0
+    echo "Sealing genesis block"
+    peptide seal
+else
+	echo "Peptide already initialized."
+fi
