480cdce892812a737f06e976a43a1102217aba2d
Derek <derek@polymerlabs.org>
2023-11-15 13:26:53 -0800

    only deploy scripts once per chain

--- ops-bedrock/deploy-vibc-contracts.sh
+++ ops-bedrock/deploy-vibc-contracts.sh
@@ -11,44 +11,46 @@ PORT_PREFIX="${2}"    # example: polyibc.op1
 POLYMER_JSON="${3}"   # example: polymer-l2-1.json
 CONTRACTS_DIR="${4}"  # packages/contracts-bedrock/lib/vibc-core-smart-contracts
 
-# deploy contracts
-pushd ${CONTRACTS_DIR}
-ESCROW_CONTRACT_ADDRESS="$(
-	forge create --json --rpc-url "${RPC_URL}" --private-key "${PRIVATE_KEY}" contracts/Escrow.sol:Escrow | \
-		jq -r .deployedTo
-)"
-VERIFIER_CONTRACT_ADDRESS="$(
-	forge create --json --rpc-url "${RPC_URL}" --private-key "${PRIVATE_KEY}" contracts/DummyVerifier.sol:DummyVerifier | \
-		jq -r .deployedTo
-)"
-OP_CONSENSUS_STATE_MANAGER_ADDRESS="$(
-	forge create --json --rpc-url "${RPC_URL}" --private-key "${PRIVATE_KEY}" \
-		 contracts/DummyConsensusStateManager.sol:DummyConsensusStateManager | \
-	jq -r .deployedTo
-)"
-DISPATCHER_CONTRACT_ADDRESS="$(
-	forge create --json --rpc-url "${RPC_URL}" --private-key "${PRIVATE_KEY}" \
-		contracts/Dispatcher.sol:Dispatcher --constructor-args "${VERIFIER_CONTRACT_ADDRESS}" \
-		"${ESCROW_CONTRACT_ADDRESS}" "${PORT_PREFIX}" "${OP_CONSENSUS_STATE_MANAGER_ADDRESS}" | \
-	jq -r .deployedTo
-)"
-RECEIVER_CONTRACT_ADDRESS="$(
-	forge create --json --rpc-url "${RPC_URL}" --private-key "${PRIVATE_KEY}" contracts/Mars.sol:Mars | \
-		jq -r .deployedTo
-)"
-popd
+if [ ! -e ${POLYMER_JSON} ]; then
+  # deploy contracts
+  pushd ${CONTRACTS_DIR}
+  ESCROW_CONTRACT_ADDRESS="$(
+    forge create --json --rpc-url "${RPC_URL}" --private-key "${PRIVATE_KEY}" contracts/Escrow.sol:Escrow | \
+      jq -r .deployedTo
+  )"
+  VERIFIER_CONTRACT_ADDRESS="$(
+    forge create --json --rpc-url "${RPC_URL}" --private-key "${PRIVATE_KEY}" contracts/DummyVerifier.sol:DummyVerifier | \
+      jq -r .deployedTo
+  )"
+  OP_CONSENSUS_STATE_MANAGER_ADDRESS="$(
+    forge create --json --rpc-url "${RPC_URL}" --private-key "${PRIVATE_KEY}" \
+      contracts/DummyConsensusStateManager.sol:DummyConsensusStateManager | \
+    jq -r .deployedTo
+  )"
+  DISPATCHER_CONTRACT_ADDRESS="$(
+    forge create --json --rpc-url "${RPC_URL}" --private-key "${PRIVATE_KEY}" \
+      contracts/Dispatcher.sol:Dispatcher --constructor-args "${VERIFIER_CONTRACT_ADDRESS}" \
+      "${ESCROW_CONTRACT_ADDRESS}" "${PORT_PREFIX}" "${OP_CONSENSUS_STATE_MANAGER_ADDRESS}" | \
+    jq -r .deployedTo
+  )"
+  RECEIVER_CONTRACT_ADDRESS="$(
+    forge create --json --rpc-url "${RPC_URL}" --private-key "${PRIVATE_KEY}" contracts/Mars.sol:Mars | \
+      jq -r .deployedTo
+  )"
+  popd
 
-# create json to store addresses
-jq -n \
-  --arg escrow "$ESCROW_CONTRACT_ADDRESS" \
-  --arg verifier "$VERIFIER_CONTRACT_ADDRESS" \
-  --arg op "$OP_CONSENSUS_STATE_MANAGER_ADDRESS" \
-  --arg dispatcher "$DISPATCHER_CONTRACT_ADDRESS" \
-  --arg receiver "$RECEIVER_CONTRACT_ADDRESS" \
-'{
-  polymer_escrow_address: $escrow,
-  polymer_verifier_address: $verifier,
-  op_consensus_state_manager_address: $op,
-  polymer_dispatcher_address: $dispatcher,
-  polymer_receiver_address: $receiver
-}' | tee "${POLYMER_JSON}"
+  # create json to store addresses
+  jq -n \
+    --arg escrow "$ESCROW_CONTRACT_ADDRESS" \
+    --arg verifier "$VERIFIER_CONTRACT_ADDRESS" \
+    --arg op "$OP_CONSENSUS_STATE_MANAGER_ADDRESS" \
+    --arg dispatcher "$DISPATCHER_CONTRACT_ADDRESS" \
+    --arg receiver "$RECEIVER_CONTRACT_ADDRESS" \
+  '{
+    polymer_escrow_address: $escrow,
+    polymer_verifier_address: $verifier,
+    op_consensus_state_manager_address: $op,
+    polymer_dispatcher_address: $dispatcher,
+    polymer_receiver_address: $receiver
+  }' | tee "${POLYMER_JSON}"
+fi
