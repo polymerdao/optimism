bb95b5be8e6a74d7682c326762c56b773f66c285
Derek <derek@polymerlabs.org>
2023-11-15 14:47:43 -0800

    fixup ibc scripts

--- ops-bedrock/scripts/create-ibc-channel.sh
+++ ops-bedrock/scripts/create-ibc-channel.sh
@@ -16,17 +16,19 @@ fi
 RELAYER_ENDPOINT="${1}" # example localhost:4001
 POLYMER_L2_1_JSON="${2}"
 POLYMER_L2_2_JSON="${3}"
+ORDERING=1
 
 receiverA="$( jq -r .polymer_receiver_address ${POLYMER_L2_1_JSON} )"
 receiverB="$( jq -r .polymer_receiver_address ${POLYMER_L2_2_JSON} )"
 
 data=$( jq -n \
-   --arg receiver "$receiverA"  \
+   --arg receiver "${receiverA}"  \
    --arg portid "polyibc.op2.${receiverB:2}" \
+   --arg ordering ${ORDERING} \
    '{
           "receiverAddress": $receiver,
           "version": "1.0",
-          "ordering": 0,
+          "ordering": $ordering|tonumber,
           "feeEnabled": false,
           "connectionHops": ["connection-2", "connection-1"],
           "counterparty": {
