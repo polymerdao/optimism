b8e2ad888f31ee7a9ad0c88c1a2dfaa88d099c5a
Derek <derek@polymerlabs.org>
2023-11-15 13:03:03 -0800

    add step to prune exited containers when resetting devnet

--- ops-bedrock/scripts/clean-devnet.sh
+++ ops-bedrock/scripts/clean-devnet.sh
@@ -7,5 +7,6 @@ set -uo pipefail
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 docker kill $(docker ps -a --filter name=^/ops-bedrock -q)
+docker container prune -f
 docker volume rm $(docker volume ls --filter='name=ops-bedrock*' -q)
 rm -rf ${SCRIPT_DIR}/../../.devnet
