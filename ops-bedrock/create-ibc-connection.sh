89a3767fd024a9b82e7ef45628b874b80efad737
Derek <derek@polymerlabs.org>
2023-11-14 21:46:10 -0800

    add/update ibc helper scripts and .gitignore

--- ops-bedrock/create-ibc-connection.sh
+++ ops-bedrock/create-ibc-connection.sh
@@ -1,8 +1,20 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+set -euo pipefail
 
 # create ibc connection
 
-CHAIN_ID="${1}"
+function usage() {
+    >&2 echo "Usage: ${0} <relayer_endpoint> <chain_id>"
+    exit 1
+}
+
+if (( $# != 2 )); then
+    usage
+fi
+
+RELAYER_ENDPOINT="${1}" # example localhost:4001
+CHAIN_ID="${2}"
 data="{\"chainID\":\"${CHAIN_ID}\"}\"
 
-curl localhost:4001/paths/op-polymer-1/createConnection -d ${data}
+curl "${RELAYER_ENDPOINT}/paths/op-polymer-1/createConnection" -d ${data}
